4b830ea6e3132e42b78c2bbdf26a6ba02dcba5da
Iswenzz <suxlolz1528@gmail.com>
2020-07-02 14:59:05 +0200

    Index: back to top button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,22 @@
 $(document).ready(() => 
 {
+	// Back to top button
+	$(window).scroll(function() 
+	{
+		if ($(this).scrollTop() > 400)
+			$('#back-to-top').fadeIn();
+		else
+			$('#back-to-top').fadeOut();
+	});
+	$('#back-to-top').click(function() 
+	{
+		$('body,html').animate({
+			scrollTop: 0
+		}, 400);
+		return false;
+	});
+
+	// Responsive slick carousel
 	$('.carousel').slick({
 		dots: true,
 		infinite: false,
